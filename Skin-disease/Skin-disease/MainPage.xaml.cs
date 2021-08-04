37c198a3f689848f68f22f8234dd0ed0c281fed7
DiaDerradji <rnoir24@gmail.com>
2021-08-05 01:35:18 +0200

    pick photos

--- Skin-disease/Skin-disease/MainPage.xaml.cs
+++ Skin-disease/Skin-disease/MainPage.xaml.cs
@@ -6,16 +6,58 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Essentials;
+using System.IO;
+
 namespace Skin_disease
 {
     public partial class MainPage : ContentPage
     {
+        string PhotoPath;
         public MainPage()
         {
+         
             InitializeComponent();
-            var result =  MediaPicker.CapturePhotoAsync();
-            
-            
+            TakePhotoAsync();
+           
+        }
+        async Task TakePhotoAsync()
+        {
+            try
+            {
+                var photo = await MediaPicker.CapturePhotoAsync();
+                await LoadPhotoAsync(photo);
+                Console.WriteLine($"CapturePhotoAsync COMPLETED: {PhotoPath}");
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Feature is not supported on the device
+            }
+            catch (PermissionException pEx)
+            {
+                // Permissions not granted
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"CapturePhotoAsync THREW: {ex.Message}");
+            }
+        }
+
+        async Task LoadPhotoAsync(FileResult photo)
+        {
+            // canceled
+            if (photo == null)
+            {
+                PhotoPath = null;
+                return;
+            }
+            // save the file into local storage
+            var newFile = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+            using (var stream = await photo.OpenReadAsync())
+            using (var newStream = File.OpenWrite(newFile))
+                await stream.CopyToAsync(newStream);
+
+            PhotoPath = newFile;
+           
         }
     }
 }
